3306d44356088500aa0275e4f41370d8818ed07a
AmoghBharadwaj2 <102871499+AmoghBharadwaj2@users.noreply.github.com>
2022-05-06 16:01:47 -0500

    modularize

---
+++ src/components/CourseList.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import { TermSelector, } from '../utilities/times';
+import { Course, getCourseTerm } from './Course'
+
+export const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] = useState([]);
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+    
+    return (
+      <>
+        <TermSelector term={term} setTerm={setTerm} />
+        <div className="course-list">
+        { 
+          termCourses.map(course =>
+            <Course key={ course.id } course={ course }
+              selected={selected} setSelected={ setSelected } 
+            />) 
+        }
+        </div>
+      </>
+    );
+  };
