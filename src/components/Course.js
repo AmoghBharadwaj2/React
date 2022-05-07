cd03cbbcdcfc1f6af6194fd6c4869195966e1591
AmoghBharadwaj2 <102871499+AmoghBharadwaj2@users.noreply.github.com>
2022-05-06 19:45:49 -0500

    storing data

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { toggle, hasConflict } from '../utilities/times';
+import { toggle, hasConflict, timeParts } from '../utilities/times';
+import { setData } from '../utilities/firebase';
 
 export const Course = ({ course, selected, setSelected }) => {
     const isSelected = selected.includes(course);
@@ -7,10 +8,12 @@ export const Course = ({ course, selected, setSelected }) => {
     const style = {
       backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
     };
+  
     return (
       <div className="card m-1 p-2" 
-        style={style}
-        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+          style={style}
+          onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}
+          onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
         <div className="card-body">
           <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
           <div className="card-text">{ course.title }</div>
@@ -20,7 +23,25 @@ export const Course = ({ course, selected, setSelected }) => {
     );
 };
 
- export const getCourseTerm = course => (
+const reschedule = async (course, meets) => {
+    if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+      try {
+        await setData(`/courses/${course.id}/meets`, meets);
+      } catch (error) {
+        alert(error);
+      }
+    }
+  };
+
+const getCourseMeetingData = course => {
+    const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+    const valid = !meets || timeParts(meets).days;
+    if (valid) return meets;
+    alert('Invalid meeting data');
+    return null;
+};
+
+export const getCourseTerm = course => (
     terms[course.id.charAt(0)]
 );
   
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -5,6 +5,11 @@ import { Course, getCourseTerm } from './Course'
 export const CourseList = ({ courses }) => {
     const [term, setTerm] = useState('Fall');
     const [selected, setSelected] = useState([]);
+    
+    if (scheduleChanged(selected, courses)) {
+        setSelected([])
+    };
+    
     const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
     
     return (
@@ -20,4 +25,8 @@ export const CourseList = ({ courses }) => {
         </div>
       </>
     );
-  };
+};
+
+const scheduleChanged = (selected, courses) => (
+    selected.some(course => course !== courses[course.id])
+);
